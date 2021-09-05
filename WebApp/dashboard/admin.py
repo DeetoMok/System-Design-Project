d24bd1e91a18f9bd8af538345d29234667b76d48
Mok Kai Sheng Daryl <54903425+DeetoMok@users.noreply.github.com>
2021-09-05 09:55:14 +0800

    added models

--- WebApp/dashboard/admin.py
+++ WebApp/dashboard/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
+from .models import CurrentAED, currentAEDFile
 
-# Register your models here.
+
+admin.site.register(CurrentAED)
+admin.site.register(currentAEDFile)
--- WebApp/dashboard/models.py
+++ WebApp/dashboard/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
 # Create your models here.
+class CurrentAED(models.Model):
+    PAS = [(i,str(i)) for i in range(1,56)]
+    REGIONS = (("N",'North'),("S",'South'),("E",'East'),("W",'West'))
+    lat = models.FloatField()
+    lon = models.FloatField()
+    country = models.CharField(max_length=200, default="singapore")
+    pa = models.IntegerField(blank=True, null=True, choices=PAS)
+    region = models.CharField(max_length=200, blank=True, null=True, choices=REGIONS)
+    subzone = models.CharField(max_length=200, blank=True, null=True)
+
+class currentAEDFile(models.Model):
+    data = models.FileField(upload_to='')
