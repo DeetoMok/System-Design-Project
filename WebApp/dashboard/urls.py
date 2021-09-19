e7c768d904d3e4fa189bbeb0c0c77f86d94976f5
Mok Kai Sheng Daryl <54903425+DeetoMok@users.noreply.github.com>
2021-09-19 21:14:30 +0800

    commit datascience codes with K means clustering

--- WebApp/dashboard/urls.py
+++ WebApp/dashboard/urls.py
@@ -1,12 +1,12 @@
 from django.urls import path, include
 from . import views
-from .views import Another, AedViewSet
+# from .views import Another, AedViewSet
 from rest_framework import routers
 
-# Create router for serializer
-router = routers.DefaultRouter()
-# register 'aeds' url to pass AedViewSet
-router.register('aeds', AedViewSet)
+# # Create router for serializer
+# router = routers.DefaultRouter()
+# # register 'aeds' url to pass AedViewSet
+# router.register('aeds', AedViewSet)
 
 urlpatterns = [
     # path('', include(router.urls)),          #path with router to a ViewSet
--- WebApp/dashboard/views.py
+++ WebApp/dashboard/views.py
@@ -31,26 +31,26 @@ def getAed(request, pk):
     serializer = AedSerializer(aeds, many=False)             #many=False means return one object0
     return Response(serializer.data)
 
-class Another(View):
-    # CurrentAED.objects.filter() will return a list
-    # CurrentAED.objects.get() will return only one record
-    currentaed = CurrentAED.objects.all()
-    output = ""
-    for aed in currentaed:
-        output += f"We have {aed.lat} latitude, {aed.lon} longtitude aed in database<br>"
+# class Another(View):
+#     # CurrentAED.objects.filter() will return a list
+#     # CurrentAED.objects.get() will return only one record
+#     currentaed = CurrentAED.objects.all()
+#     output = ""
+#     for aed in currentaed:
+#         output += f"We have {aed.lat} latitude, {aed.lon} longtitude aed in database<br>"
 
-    def get(self, request):
-        return HttpResponse(self.output)
+#     def get(self, request):
+#         return HttpResponse(self.output)
 
 
-# def first(request):
-#     return HttpResponse("First message from views")
+# # def first(request):
+# #     return HttpResponse("First message from views")
 
-class AedViewSet(viewsets.ModelViewSet):
-    serializer_class = AedSerializer
-    queryset = CurrentAED.objects.all()
-    # Add token authentication to allow query
-    authentication_classes = (TokenAuthentication, )
-    # Require authentication to access this model. Change to IsAny to let all user access this ViewSet
-    # permission_classes = (IsAuthenticated,)
+# class AedViewSet(viewsets.ModelViewSet):
+#     serializer_class = AedSerializer
+#     queryset = CurrentAED.objects.all()
+#     # Add token authentication to allow query
+#     authentication_classes = (TokenAuthentication, )
+#     # Require authentication to access this model. Change to IsAny to let all user access this ViewSet
+#     # permission_classes = (IsAuthenticated,)
 
