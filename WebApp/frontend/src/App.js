5bb977cf4adb4a757e96307f78758538e1daf8fb
Mok Kai Sheng Daryl <54903425+DeetoMok@users.noreply.github.com>
2021-09-19 20:42:56 +0800

    imported template

--- WebApp/frontend/src/App.js
+++ WebApp/frontend/src/App.js
@@ -1,10 +1,14 @@
 import './App.css';
 import Header from './components/Header'
+import Toolbar from './components/Toolbar'
+import Dashboard from './templates/Dashboard'
 
 function App() {
   return (
     <div className="App">
-      <Header />
+      {/* <Toolbar /> */}
+      {/* <Header /> */}
+      <Dashboard />
     </div>
   );
 }
---
+++ WebApp/frontend/src/components/Dashboard.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import Typography from '@material-ui/core/Typography'
+
+const Dashboard = () => {
+    return (
+        <div>
+            <Typography component="h2" variant="h6" color="primary" gutterBottom>
+            DASHHBOARD
+            </Typography>
+        </div>
+    )
+}
+
+export default Dashboard
