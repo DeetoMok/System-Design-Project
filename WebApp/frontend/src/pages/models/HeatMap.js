b73db59d5e7a1db1583303526f3e57ba824712d9
Mok Kai Sheng Daryl <54903425+DeetoMok@users.noreply.github.com>
2022-03-18 01:04:38 +0800

    update

--- WebApp/frontend/src/pages/models/HeatMap.js
+++ WebApp/frontend/src/pages/models/HeatMap.js
@@ -23,13 +23,224 @@ function valueLabelFormat(value) {
 function HeatMap() {
 
   const mapRef = useRef();
-  const [map, setMap] = React.useState();
-  const [maps, setMaps] = React.useState();
-
+  const [zoom, setZoom] = useState(11.5);
+  const [pan, setPan] = useState({ lat: 1.3599614835747427, lng: 103.82221222898332 }); 
+  const sumValues = obj => Object.values(obj).reduce((a, b) => a + b);
   const handleApiLoaded2010 = (map, maps) => {
     heatMapData.map((subzone) => {
       var coords = subzone.coordinates;
-      let popDensity = subzone.year['2010'].population_density;
+      let population = sumValues(subzone.year['2010'].gender)
+      let area = subzone.area;
+      let popDensity = population/area;
+      if (popDensity == 0) {
+        var subzoneArea = new maps.Polygon({
+          paths: coords,
+          strokeColor: "#000000",
+          strokeOpacity: 0.8,
+          strokeWeight: 1,
+          fillColor: "#FFFFFF",
+          fillOpacity: 0.7,
+          
+          });
+      } else if (popDensity <= 100) {
+        // green 0-100
+        var subzoneArea = new maps.Polygon({
+          paths: coords,
+          strokeColor: "#000000",
+          strokeOpacity: 0.8,
+          strokeWeight: 1,
+          fillColor: "#5fe60b",
+          fillOpacity: 0.3+(popDensity/100),
+          
+          });
+      } else if (popDensity <= 500){
+        // yellow 100 - 500
+        var subzoneArea = new maps.Polygon({
+          paths: coords,
+          strokeColor: "#000000",
+          strokeOpacity: 0.8,
+          strokeWeight: 1,
+          fillColor: "#e6e60b",
+          fillOpacity: 0.3+(popDensity/500),
+          
+          });
+      } else if (popDensity <= 3500){
+        // orange 500 - 3.5k
+        var subzoneArea = new maps.Polygon({
+          paths: coords,
+          strokeColor: "#000000",
+          strokeOpacity: 0.8,
+          strokeWeight: 1,
+          fillColor: "#e6870b",
+          fillOpacity: 0.3+(popDensity/3500),
+          
+          });
+      } else if (popDensity <= 20000){
+        // red 3.5 - 20k
+        var subzoneArea = new maps.Polygon({
+          paths: coords,
+          strokeColor: "#00000",
+          strokeOpacity: 0.8,
+          strokeWeight: 1,
+          fillColor: "#d61a1a",
+          fillOpacity: 0.3+(popDensity/2000),
+          
+          });
+      } else{
+        var subzoneArea = new maps.Polygon({
+          paths: coords,
+          strokeColor: "#000000",
+          strokeOpacity: 0.8,
+          strokeWeight: 1,
+          fillColor: "#f242f5",
+          fillOpacity: 0.3+(popDensity/35000),
+          
+          });
+      }
+      let maxLat = 0;
+      let maxLatIndex = 0;
+      for (let i=0; i < subzone.coordinates.length; i++) {
+        if (subzone.coordinates[i].lat>maxLat) {
+          maxLat = subzone.coordinates[i];
+          maxLatIndex = i;
+          
+        }
+      }
+      var infoLatlng = new maps.LatLng(subzone.coordinates[maxLatIndex].lat, subzone.coordinates[maxLatIndex].lng);
+      
+      let contentString = "<b>"+ subzone.subzone_name +"</b><br>" +
+      "Region: "+ subzone.region +" <br>" +
+      "Population Density: "+ popDensity.toFixed(2) +" <br>" +
+      "<br>";
+
+      var infoWindow =  new maps.InfoWindow({
+        content: contentString,
+        map: map,
+        position: infoLatlng
+      });  
+
+      subzoneArea.setMap(map);
+      infoWindow.close()
+      subzoneArea.addListener("mouseover", ()=> {
+        infoWindow.open(map, this);
+      });
+      subzoneArea.addListener("mouseout", () => {
+        infoWindow.close();
+      })
+
+    })
+  }
+  const handleApiLoaded2020 = (map, maps) => {
+    heatMapData.map((subzone) => {
+      var coords = subzone.coordinates;
+      let population = sumValues(subzone.year['2020'].gender)
+      let area = subzone.area;
+      let popDensity = population/area;
+      if (popDensity == 0) {
+        var subzoneArea = new maps.Polygon({
+          paths: coords,
+          strokeColor: "#000000",
+          strokeOpacity: 0.8,
+          strokeWeight: 1,
+          fillColor: "#FFFFFF",
+          fillOpacity: 0.7,
+          
+          });
+      } else if (popDensity <= 100) {
+        // green 0-100
+        var subzoneArea = new maps.Polygon({
+          paths: coords,
+          strokeColor: "#000000",
+          strokeOpacity: 0.8,
+          strokeWeight: 1,
+          fillColor: "#5fe60b",
+          fillOpacity: 0.3+(popDensity/100),
+          
+          });
+      } else if (popDensity <= 500){
+        // yellow 100 - 500
+        var subzoneArea = new maps.Polygon({
+          paths: coords,
+          strokeColor: "#000000",
+          strokeOpacity: 0.8,
+          strokeWeight: 1,
+          fillColor: "#e6e60b",
+          fillOpacity: 0.3+(popDensity/500),
+          
+          });
+      } else if (popDensity <= 3500){
+        // orange 500 - 3.5k
+        var subzoneArea = new maps.Polygon({
+          paths: coords,
+          strokeColor: "#000000",
+          strokeOpacity: 0.8,
+          strokeWeight: 1,
+          fillColor: "#e6870b",
+          fillOpacity: 0.3+(popDensity/3500),
+          
+          });
+      } else if (popDensity <= 20000){
+        // red 3.5 - 20k
+        var subzoneArea = new maps.Polygon({
+          paths: coords,
+          strokeColor: "#00000",
+          strokeOpacity: 0.8,
+          strokeWeight: 1,
+          fillColor: "#d61a1a",
+          fillOpacity: 0.3+(popDensity/2000),
+          
+          });
+      } else{
+        var subzoneArea = new maps.Polygon({
+          paths: coords,
+          strokeColor: "#000000",
+          strokeOpacity: 0.8,
+          strokeWeight: 1,
+          fillColor: "#f242f5",
+          fillOpacity: 0.3+(popDensity/35000),
+          
+          });
+      }
+      let maxLat = 0;
+      let maxLatIndex = 0;
+      for (let i=0; i < subzone.coordinates.length; i++) {
+        if (subzone.coordinates[i].lat>maxLat) {
+          maxLat = subzone.coordinates[i];
+          maxLatIndex = i;
+          
+        }
+      }
+      var infoLatlng = new maps.LatLng(subzone.coordinates[maxLatIndex].lat, subzone.coordinates[maxLatIndex].lng);
+
+      
+      let contentString = "<b>"+ subzone.subzone_name +"</b><br>" +
+      "Region: "+ subzone.region +" <br>" +
+      "Population Density: "+ popDensity.toFixed(2) +" <br>" +
+      "<br>";
+
+      var infoWindow =  new maps.InfoWindow({
+        content: contentString,
+        map: map,
+        position: infoLatlng
+      });  
+
+      subzoneArea.setMap(map);
+      infoWindow.close()
+      subzoneArea.addListener("mouseover", ()=> {
+        infoWindow.open(map, this);
+      });
+      subzoneArea.addListener("mouseout", () => {
+        infoWindow.close();
+      })
+
+    })
+  } 
+  const handleApiLoaded2015 = (map, maps) => { 
+    heatMapData.map((subzone) => {
+      var coords = subzone.coordinates;
+      let population = sumValues(subzone.year['2015'].gender)
+      let area = subzone.area;
+      let popDensity = population/area;
       if (popDensity == 0) {
         var subzoneArea = new maps.Polygon({
           paths: coords,
@@ -129,220 +340,13 @@ function HeatMap() {
 
     })
   }
-  const handleApiLoaded2020 = (map, maps) => {
-    heatMapData.map((subzone) => {
-      var coords = subzone.coordinates;
-      let popDensity = subzone.year['2020'].population_density;
-      if (popDensity == 0) {
-        var subzoneArea = new maps.Polygon({
-          paths: coords,
-          strokeColor: "#000000",
-          strokeOpacity: 0.8,
-          strokeWeight: 1,
-          fillColor: "#FFFFFF",
-          fillOpacity: 0.7,
-          
-          });
-      } else if (popDensity <= 100) {
-        // green 0-100
-        var subzoneArea = new maps.Polygon({
-          paths: coords,
-          strokeColor: "#000000",
-          strokeOpacity: 0.8,
-          strokeWeight: 1,
-          fillColor: "#5fe60b",
-          fillOpacity: 0.7,
-          
-          });
-      } else if (popDensity <= 500){
-        // yellow 100 - 500
-        var subzoneArea = new maps.Polygon({
-          paths: coords,
-          strokeColor: "#000000",
-          strokeOpacity: 0.8,
-          strokeWeight: 1,
-          fillColor: "#e6e60b",
-          fillOpacity: 0.7,
-          
-          });
-      } else if (popDensity <= 3500){
-        // orange 500 - 3.5k
-        var subzoneArea = new maps.Polygon({
-          paths: coords,
-          strokeColor: "#000000",
-          strokeOpacity: 0.8,
-          strokeWeight: 1,
-          fillColor: "#e6870b",
-          fillOpacity: 0.7,
-          
-          });
-      } else if (popDensity <= 20000){
-        // red 3.5 - 20k
-        var subzoneArea = new maps.Polygon({
-          paths: coords,
-          strokeColor: "#00000",
-          strokeOpacity: 0.8,
-          strokeWeight: 1,
-          fillColor: "#d61a1a",
-          fillOpacity: 0.7,
-          
-          });
-      } else{
-        var subzoneArea = new maps.Polygon({
-          paths: coords,
-          strokeColor: "#000000",
-          strokeOpacity: 0.8,
-          strokeWeight: 1,
-          fillColor: "#f242f5",
-          fillOpacity: 0.7,
-          
-          });
-      }
-      let maxLat = 0;
-      let maxLatIndex = 0;
-      for (let i=0; i < subzone.coordinates.length; i++) {
-        if (subzone.coordinates[i].lat>maxLat) {
-          maxLat = subzone.coordinates[i];
-          maxLatIndex = i;
-          
-        }
-      }
-      var infoLatlng = new maps.LatLng(subzone.coordinates[maxLatIndex].lat, subzone.coordinates[maxLatIndex].lng);
 
-      
-      let contentString = "<b>"+ subzone.subzone_name +"</b><br>" +
-      "Region: "+ subzone.region +" <br>" +
-      "Population Density: "+ popDensity.toFixed(2) +" <br>" +
-      "<br>";
-
-      var infoWindow =  new maps.InfoWindow({
-        content: contentString,
-        map: map,
-        position: infoLatlng
-      });  
-
-      subzoneArea.setMap(map);
-      infoWindow.close()
-      subzoneArea.addListener("mouseover", ()=> {
-        infoWindow.open(map, this);
-      });
-      subzoneArea.addListener("mouseout", () => {
-        infoWindow.close();
-      })
-
-    })
-  } 
-  const handleApiLoaded2015 = (map, maps) => {
-    heatMapData.map((subzone) => {
-      var coords = subzone.coordinates;
-      let popDensity = subzone.year['2015'].population_density;
-      if (popDensity == 0) {
-        var subzoneArea = new maps.Polygon({
-          paths: coords,
-          strokeColor: "#000000",
-          strokeOpacity: 0.8,
-          strokeWeight: 1,
-          fillColor: "#FFFFFF",
-          fillOpacity: 0.7,
-          
-          });
-      } else if (popDensity <= 100) {
-        // green 0-100
-        var subzoneArea = new maps.Polygon({
-          paths: coords,
-          strokeColor: "#000000",
-          strokeOpacity: 0.8,
-          strokeWeight: 1,
-          fillColor: "#5fe60b",
-          fillOpacity: 0.7,
-          
-          });
-      } else if (popDensity <= 500){
-        // yellow 100 - 500
-        var subzoneArea = new maps.Polygon({
-          paths: coords,
-          strokeColor: "#000000",
-          strokeOpacity: 0.8,
-          strokeWeight: 1,
-          fillColor: "#e6e60b",
-          fillOpacity: 0.7,
-          
-          });
-      } else if (popDensity <= 3500){
-        // orange 500 - 3.5k
-        var subzoneArea = new maps.Polygon({
-          paths: coords,
-          strokeColor: "#000000",
-          strokeOpacity: 0.8,
-          strokeWeight: 1,
-          fillColor: "#e6870b",
-          fillOpacity: 0.7,
-          
-          });
-      } else if (popDensity <= 20000){
-        // red 3.5 - 20k
-        var subzoneArea = new maps.Polygon({
-          paths: coords,
-          strokeColor: "#00000",
-          strokeOpacity: 0.8,
-          strokeWeight: 1,
-          fillColor: "#d61a1a",
-          fillOpacity: 0.7,
-          
-          });
-      } else{
-        var subzoneArea = new maps.Polygon({
-          paths: coords,
-          strokeColor: "#000000",
-          strokeOpacity: 0.8,
-          strokeWeight: 1,
-          fillColor: "#f242f5",
-          fillOpacity: 0.7,
-          
-          });
-      }
-      let maxLat = 0;
-      let maxLatIndex = 0;
-      for (let i=0; i < subzone.coordinates.length; i++) {
-        if (subzone.coordinates[i].lat>maxLat) {
-          maxLat = subzone.coordinates[i];
-          maxLatIndex = i;
-          
-        }
-      }
-      var infoLatlng = new maps.LatLng(subzone.coordinates[maxLatIndex].lat, subzone.coordinates[maxLatIndex].lng);
-
-      
-      let contentString = "<b>"+ subzone.subzone_name +"</b><br>" +
-      "Region: "+ subzone.region +" <br>" +
-      "Population Density: "+ popDensity.toFixed(2) +" <br>" +
-      "<br>";
-
-      var infoWindow =  new maps.InfoWindow({
-        content: contentString,
-        map: map,
-        position: infoLatlng
-      });  
-
-      subzoneArea.setMap(map);
-      infoWindow.close()
-      subzoneArea.addListener("mouseover", ()=> {
-        infoWindow.open(map, this);
-      });
-      subzoneArea.addListener("mouseout", () => {
-        infoWindow.close();
-      })
-
-    })
-  }
-
-  const [snapshotYear, setSnapshotYear] = React.useState(2015);
+  const [snapshotYear, setSnapshotYear] = React.useState(2010);
 
   const handleChange = (event, newValue) => {
     if (typeof newValue === 'number') {
       setSnapshotYear(newValue);
     }
-    // console.log(map);
   };
 
   return (
@@ -353,10 +357,15 @@ function HeatMap() {
           bootstrapURLKeys={{
               key: 'AIzaSyCsATUmU17pbVtJlvkLZwuEpTxafin92II'
           }}
-          defaultCenter={{ lat: 1.3599614835747427, lng: 103.82221222898332 }}
-          defaultZoom={11.5}
+          defaultCenter={{ lat: pan.lat, lng: pan.lng }}
+          defaultZoom={zoom}
+          
           yesIWantToUseGoogleMapApiInternals
           onGoogleApiLoaded={({ map, maps}) => handleApiLoaded2010(map,maps)}
+          onChange={({zoom, center}) => {
+            setPan({lat: center.lat, lng: center.lng});
+            setZoom(zoom);
+          }}
           >
             <Polyline 
             />
@@ -367,10 +376,14 @@ function HeatMap() {
           bootstrapURLKeys={{
               key: 'AIzaSyCsATUmU17pbVtJlvkLZwuEpTxafin92II'
           }}
-          defaultCenter={{ lat: 1.3599614835747427, lng: 103.82221222898332 }}
-          defaultZoom={11.5}
+          defaultCenter={{ lat: pan.lat, lng: pan.lng }}
+          defaultZoom={zoom}
           yesIWantToUseGoogleMapApiInternals
           onGoogleApiLoaded={({ map, maps}) => handleApiLoaded2015(map,maps)}
+          onChange={({zoom, center}) => {
+            setPan({lat: center.lat, lng: center.lng});
+            setZoom(zoom);
+          }} 
           >
             <Polyline 
             />
@@ -382,10 +395,14 @@ function HeatMap() {
           bootstrapURLKeys={{
               key: 'AIzaSyCsATUmU17pbVtJlvkLZwuEpTxafin92II'
           }}
-          defaultCenter={{ lat: 1.3599614835747427, lng: 103.82221222898332 }}
-          defaultZoom={11.5}
+          defaultCenter={{ lat: pan.lat, lng: pan.lng }}
+          defaultZoom={zoom}
           yesIWantToUseGoogleMapApiInternals
           onGoogleApiLoaded={({ map, maps}) => handleApiLoaded2020(map,maps)}
+          onChange={({zoom, center}) => {
+            setPan({lat: center.lat, lng: center.lng});
+            setZoom(zoom);
+          }}      
           >
             <Polyline 
             />
